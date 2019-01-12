ace28842a234806b589bd42eb46fcc9be1197b2e
Maxghost33 <maxim.jovanik@gmail.com>
2019-01-12 10:39:22 -0500

    Successfull Testing Error

--- pylint-test.py
+++ pylint-test.py
@@ -8,7 +8,7 @@ def main():
 
 def check_rate_code():
     results = Run([sys.argv[1]], do_exit=False)
-    if (results.linter.stats['global_note'] <= 7):
+    if (results.linter.stats['global_note'] <= 5):
         raise Exception("Code rate smaller than standard")
         return 1
     else:
