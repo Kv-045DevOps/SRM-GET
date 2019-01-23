f4afa239deef87cceea09d54deb46381c8f7b185
Maxghost33 <maxim.jovanik@gmail.com>
2019-01-23 17:22:45 -0500

    Change e2e testing namespace testing

--- e2e-test-test.py
+++ e2e-test-test.py
@@ -7,18 +7,18 @@ def main():
 
 
 def check_services():
-    URL = "ui-service.testing.svc:5000"
+    URL = "http://ui-service.testing.svc:5000"
     req = requests.get(URL)
     if req.status_code == 200:
-        print("Tests successfull pass")
+        print("Test UI successfull pass")
         continue
     else:
         raise Exception("Fucking bitch!!!! Opssss")
         return 1
-    URL_GET = "ui-service.testing.svc:5000/salaries"
+    URL_GET = "http://ui-service.testing.svc:5000/salaries"
     reques = requests.get(URL_GET)
     if reques.status_code == 200:
-        print("Get req success to UI")
+        print("UI success to GET")
         return 0
     else:
         raise Exception("Fucking bitch again!!!! Opssss. You are unclever animal")
