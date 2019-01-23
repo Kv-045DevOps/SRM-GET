4ec04f6c0910dfc6d931da7e1cf72ed1e0ad43f0
Maxghost33 <maxim.jovanik@gmail.com>
2019-01-23 16:59:33 -0500

    Indian Code#3

--- sed_python_test.py
+++ sed_python_test.py
@@ -6,7 +6,8 @@ with open(sys.argv[1], 'r') as file:
     arr_f = ["100.71.71.71:5000/get-service", "100.71.71.71:5000/post-service", "100.71.71.71:5000/ui-service", "100.71.71.71:5000/db-service", "100.71.71.71:5000/init-container"]
     arr = ["100.71.71.71:5000/get-service:v2", "100.71.71.71:5000/post-service:2.1", "100.71.71.71:5000/ui-service:latest", "100.71.71.71:5000/db-service:latest", "100.71.71.71:5000/init-container:latest"]
     data = file.read()
-    for i in range(0,len(arr_f)):
+    print(len(arr_f))
+    for i in range(len(arr_f)-1):
         tmp = arr_f[i] + ":" + sys.argv[2]
         data = data.replace(arr[i], tmp)
     
