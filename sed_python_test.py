def0adcb5d6a457ba607d1a099955b611448a860
Maxghost33 <maxim.jovanik@gmail.com>
2019-01-23 16:53:04 -0500

    Indian Code#2

--- sed_python_test.py
+++ sed_python_test.py
@@ -3,7 +3,7 @@ import sys
 
 with open(sys.argv[1], 'r') as file:
     str_tmp = "ghostgoose"
-    arr_f = arr = ["100.71.71.71:5000/get-service", "100.71.71.71:5000/post-service", "100.71.71.71:5000/ui-service", "100.71.71.71:5000/db-service", "100.71.71.71:5000/init-container"]
+    arr_f = ["100.71.71.71:5000/get-service", "100.71.71.71:5000/post-service", "100.71.71.71:5000/ui-service", "100.71.71.71:5000/db-service", "100.71.71.71:5000/init-container"]
     arr = ["100.71.71.71:5000/get-service:v2", "100.71.71.71:5000/post-service:2.1", "100.71.71.71:5000/ui-service:latest", "100.71.71.71:5000/db-service:latest", "100.71.71.71:5000/init-container:latest"]
     data = file.read()
     for i in range(0,len(arr_f)-1):
