44d1782f9452cdac695e84310e83fa6da7c35d26
Maxghost33 <maxim.jovanik@gmail.com>
2019-02-01 07:15:36 -0500

    Check registry tag

--- images-registry-test.py
+++ images-registry-test.py
@@ -8,14 +8,15 @@ def main():
 
 
 def check_image():
-   tmp = requests.get("http://" + sys.argv[1] + "/v2/" + sys.argv[3] + "/tags/list")
-   req = tmp.json()
-   print(req)
-   if req["name"] == sys.argv[2] and sys.argv[3] in req["tags"]:
-       return 0
-   else:
-       raise Exception("Image with tag " + sys.argv[3] + " does not exist in Docker Registry with IP: " + sys.argv[1])
-       return 1
+    tmp = requests.get("http://100.71.71.71:5000/v2/" + sys.argv[1] + "/tags/list")
+    req = tmp.json()
+    print(req)
+    if req["name"] == sys.argv[1] and sys.argv[2] in req["tags"]:
+        print("First test good!!!")
+        return 0
+    else:
+        raise Exception("Image with tag " + sys.argv[1] + " does not exist in Docker Registry with IP: " + sys.argv[1])
+        return 1
 
 
 if __name__=='__main__':
