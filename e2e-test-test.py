51d55050111d34abf30b75fa2fb30f6c6dec7a0c
Maxghost33 <maxim.jovanik@gmail.com>
2019-02-03 12:02:54 -0500

    Stage 3 - Test after deploy

--- e2e-test-test.py
+++ e2e-test-test.py
@@ -13,15 +13,12 @@ def check_services():
         print("Test UI successfull pass")
     else:
         raise Exception("Fucking bitch!!!! Opssss")
-        return 1
     URL_GET = "http://ui-service.testing.svc:5000/salaries"
     reques = requests.get(URL_GET)
     if reques.status_code == 200:
         print("UI success to GET")
-        return 0
     else:
         raise Exception("Fucking bitch again!!!! Opssss. You are unclever animal")
-        return -1
         
 if __name__ == '__main__':
     main()
