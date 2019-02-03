d1223c81bd9f5135bf4f7c6ca768c01da53eee0b
Maxghost33 <maxim.jovanik@gmail.com>
2019-02-03 13:20:35 -0500

    Last Test

--- e2e-test-prod.py
+++ e2e-test-prod.py
@@ -14,14 +14,6 @@ def check_services():
         print("Tests UI successfull pass")
     else:
         raise Exception("Fucking bitch!!!! Opssss")
-    URL_GET = "http://ui-service.production.svc:5000/salaries"
-    time.sleep(5)
-    reques = requests.get(URL_GET)
-    if reques.status_code == 200:
-        print("Test success to UI-GET/salaries")
-    else:
-        print(reques.status_code)
-        raise Exception("Fucking bitch again!!!! Opssss. You are ....")
         
 if __name__ == '__main__':
     main()
